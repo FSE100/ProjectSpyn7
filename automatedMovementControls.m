6d34710679355c72ae36ba9c46655a0f6069c876
jwolf11 <57539621+jwolf11@users.noreply.github.com>
2019-11-08 10:37:58 -0700

    Add files via upload
    Updated as of 08NOV2019

---
+++ automatedMovementControls.m
@@ -0,0 +1,72 @@
+clc;
+brick = ConnectBrick('EB3');
+brick.playTone(100, 200, 300);
+
+while 1  
+    switch key
+        case 'q'
+            brick.StopMotor('A');
+            brick.StopMotor('D');
+            brick.StopMotor('B');
+            break;
+    end
+    
+     brick.MoveMotor('D', -100);
+     brick.MoveMotor('A', -100);
+    
+     if (brick.TouchPressed(3)==1) %turns left 90 if touched 
+         brick.StopMotor('A');
+         brick.StopMotor('D');
+         pause(1);
+         brick.MoveMotorAngleRel('D', 100, 690, 'Brake');
+         brick.MoveMotorAngleRel('A', -100, 690, 'Brake');
+     elseif (brick.UltrasonicDist(4)>25) %turns right 90 if touched
+         brick.StopMotor('A');
+         brick.StopMotor('D');
+         pause(1);
+         brick.MoveMotorAngleRel('D', -100, 690, 'Brake');
+         brick.MoveMotorAngleRel('A', 100, 690, 'Brake');
+     elseif (brick.ColorCode(1)==5) %red
+         pause(4);
+     elseif (brick.ColorCode(1)==3) %green
+         brick.StopMotor('A');
+         brick.StopMotor('D');
+         brick.StopMotor('B');
+         switch key
+           case 'uparrow'
+               disp('Up arrow pressed');
+               brick.MoveMotor('D', -100);
+               brick.MoveMotor('A', -100);
+           case 'downarrow'
+               disp('Down arrow pressed');
+               brick.MoveMotor('D', 100);
+               brick.MoveMotor('A', 100);
+           case 'leftarrow'
+               disp('Left arrow pressed');
+               brick.MoveMotor('D', 100);
+               brick.MoveMotor('A', -100);
+           case 'rightarrow'
+               disp('Right arrow pressed');
+               brick.MoveMotor('D', -100);
+               brick.MoveMotor('A', 100);
+           case 'w'
+               brick.MoveMotor('B', -25);
+           case 's'
+               brick.MoveMotor('B', 25);
+           case 0
+               disp('No Key pressed');
+               brick.StopMotor('A');
+               brick.StopMotor('D');
+               brick.StopMotor('B');
+           case 'q'
+               brick.StopMotor('A');
+               brick.StopMotor('D');
+               brick.StopMotor('B');
+               break;
+          end
+     elseif (brick.ColorCode(1)==2) %blue
+     else
+         continue;
+     end  
+            
+end
